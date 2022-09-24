e65e62d5ad16751912828141126d8aee8b0964e0
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-09-24 18:53:32 +0530

    Time: 150 ms (44.77%), Space: 43.1 MB (56.54%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[][] memo;
+    Integer[][] memo;
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n+2];
@@ -7,17 +7,18 @@ class Solution {
         for(int i = 1; i <= n; i++){
             arr[i] = nums[i-1];
         }
-        memo = new int[n + 1][n + 1];
-        for(int[] row : memo){
-            Arrays.fill(row, -1);
-        }
+        memo = new Integer[n + 1][n + 1];
+        // for(int[] row : memo){
+        //     Arrays.fill(row, -1);
+        // }
         
         return solve(arr, 1, n);
     }
     
     int solve(int[] nums, int left, int right){
         if(left > right) return 0;
-        if(memo[left][right] != -1){
+        
+        if(memo[left][right] != null){
             return memo[left][right];
         }
         
