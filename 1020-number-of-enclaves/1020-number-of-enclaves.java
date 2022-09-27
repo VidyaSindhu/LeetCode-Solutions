a8eeb2e01cfc25cd5661da18217cce94e63d2444
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-09-27 10:47:54 +0530

    Time: 23 ms (14.86%), Space: 68.9 MB (44.06%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,5 +1,4 @@
 class Solution {
-    int color = 2;
     final int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public int numEnclaves(int[][] grid) {
         
@@ -20,15 +19,11 @@ class Solution {
                 
             }
         }
-        // System.out.println(ans);
+
         while(!queue.isEmpty()){
             int[] top = queue.poll();
             int x = top[0];
             int y = top[1];
-            
-//             if(grid[x][y] != 1) continue;
-            
-//             grid[x][y] = -1;
             
             for(int[] dir : dirs){
                 int newX = x + dir[0];
@@ -41,35 +36,6 @@ class Solution {
                 queue.push(new int[]{newX, newY});
             }
         }
-        
-//         int ans = 0;
-//         for(int i = 0; i < grid.length; i++){
-//             for(int j = 0; j < grid[0].length; j++){
-//                 if(grid[i][j] != 0){
-//                     if(set.contains(grid[i][j])){
-//                         ans++;
-//                     }
-//                     total++;
-//                 }
-                
-//             }
-//         }
-        
         return ans;
-    }
-    
-    void dfs(int[][] grid, int x, int y, Set<Integer> set){
-        if(x == 0 || x == grid.length-1 || y == 0 || y == grid[0].length-1){
-            set.add(color);
-        }
-        
-        grid[x][y] = color;
-        for(int[] dir : dirs){
-            int newX = x + dir[0];
-            int newY = y + dir[1];
-            if(newX < 0 || newX >= grid.length || newY < 0 || newY >= grid[0].length || grid[newX][newY] != 1) continue;
-            dfs(grid, newX, newY, set);
-        }
-        
     }
 }
