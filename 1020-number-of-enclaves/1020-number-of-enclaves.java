2965eb26d50cdcf6a7771461c2bdf0c5eb2581f2
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-09-27 10:47:28 +0530

    Time: 13 ms (47.04%), Space: 73.9 MB (28.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,32 +2,60 @@ class Solution {
     int color = 2;
     final int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public int numEnclaves(int[][] grid) {
-        Set<Integer> set = new HashSet();
+        
+        Deque<int[]> queue = new ArrayDeque();
+        
+        int ans = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 1){
-                    dfs(grid, i, j, set);
-                    color++;
-                }
-                
-            }
-        }
-        
-        int ans = 0;
-        int total = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] != 0){
-                    if(set.contains(grid[i][j])){
+                    if(i == 0 || i == grid.length-1 || j == 0 || j == grid[0].length-1){
+                        queue.push(new int[]{i, j});
+                        grid[i][j] = -1;
+                    }
+                    else{
                         ans++;
                     }
-                    total++;
                 }
                 
             }
         }
+        // System.out.println(ans);
+        while(!queue.isEmpty()){
+            int[] top = queue.poll();
+            int x = top[0];
+            int y = top[1];
+            
+//             if(grid[x][y] != 1) continue;
+            
+//             grid[x][y] = -1;
+            
+            for(int[] dir : dirs){
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                
+                if(newX < 0 || newX >= grid.length || 
+                   newY < 0 || newY >= grid[0].length || grid[newX][newY] != 1) continue;
+                grid[newX][newY] = -1;
+                ans--;
+                queue.push(new int[]{newX, newY});
+            }
+        }
         
-        return total - ans;
+//         int ans = 0;
+//         for(int i = 0; i < grid.length; i++){
+//             for(int j = 0; j < grid[0].length; j++){
+//                 if(grid[i][j] != 0){
+//                     if(set.contains(grid[i][j])){
+//                         ans++;
+//                     }
+//                     total++;
+//                 }
+                
+//             }
+//         }
+        
+        return ans;
     }
     
     void dfs(int[][] grid, int x, int y, Set<Integer> set){
