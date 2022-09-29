959c708026fffecdec29eab1fa68e272db93898c
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-09-29 22:12:02 +0530

    Time: 67 ms (15.65%), Space: 43.6 MB (26.18%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -2,33 +2,38 @@ class Solution {
     public int numTeams(int[] rating) {
         int ans = 0;
         int n = rating.length;
-        int leftSmaller = 0, rightSmaller = 0;
-        int leftLarger = 0, rightLarger = 0;
-        
-        for(int i = 0; i < n; i++){
-            leftSmaller = 0;
-            rightSmaller = 0;
-            leftLarger = 0;
-            rightLarger = 0;
+        int[] smallerRatingLeft = new int[n];
+        for(int i = 0; i < n-1; i++){
             for(int j = i+1; j < n; j++){
                 if(rating[i] < rating[j]){
-                    rightLarger++;
-                }
-                if(rating[i] > rating[j]){
-                    rightSmaller++;
+                    smallerRatingLeft[i]++;
                 }
             }
-            
-            for(int j = 0; j < i; j++){
+        }
+        
+        int[] smallerRatingRight = new int[n];
+        for(int i = n-1; i >= 1; i--){
+            for(int j = i-1; j >= 0; j--){
                 if(rating[i] < rating[j]){
-                    leftLarger++;
-                }
-                if(rating[i] > rating[j]){
-                    leftSmaller++;
+                    smallerRatingRight[i]++;
                 }
             }
-            
-            ans += leftLarger * rightSmaller + leftSmaller * rightLarger;
+        }
+        
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(rating[i] < rating[j]){
+                    ans += smallerRatingLeft[j];
+                }
+            }
+        }
+        
+        for(int i = n-1; i >= 1; i--){
+            for(int j = i-1; j >= 0; j--){
+                if(rating[i] < rating[j]){
+                    ans += smallerRatingRight[j];
+                }
+            }
         }
         
         return ans;
