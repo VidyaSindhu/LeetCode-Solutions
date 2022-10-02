cedc729c21fe2faa47db4565b422cd8b72ac5cb2
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-02 19:00:47 +0530

    Time: 680 ms (18.32%), Space: 189.1 MB (7.41%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        List<Pair<Integer, Integer>> arr = new ArrayList();
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < n; i++){
+            Pair<Integer, Integer> pair = new Pair(nums[i], i);
+            arr.add(pair);
+            res.add(0);
+        }
+        
+        mergeSort(arr, 0, n-1, res);
+        
+        return res;
+    }
+    
+    void mergeSort(List<Pair<Integer, Integer>> arr, int left, int right, List<Integer> res){
+        if(left < right){
+            int mid = left + (right-left)/2;
+            mergeSort(arr, left, mid, res);
+            mergeSort(arr, mid + 1, right, res);
+            merge(arr, left, right, res);
+        }
+    }
+    
+    void merge(List<Pair<Integer, Integer>> arr, int left, int right, List<Integer> res){
+        int mid = left + (right-left)/2;
+        int i = left, j = mid + 1;
+        List<Pair<Integer, Integer>> tmp = new ArrayList();
+        int rightCount = 0;
+        while(i <= mid && j <= right){
+            if(arr.get(i).getKey() <= arr.get(j).getKey()){
+                int count = res.get(arr.get(i).getValue());
+                res.set(arr.get(i).getValue(), count + rightCount);
+                tmp.add(arr.get(i));
+                i++;
+            }
+            else{
+                rightCount++;
+                tmp.add(arr.get(j));
+                j++;
+            }
+        }
+        
+        while(i <= mid){
+            int count = res.get(arr.get(i).getValue());
+            res.set(arr.get(i).getValue(), count + rightCount);
+            tmp.add(arr.get(i));
+            i++;
+        }
+        
+        while(j <= right){
+            tmp.add(arr.get(j));
+            j++;
+        }
+        
+        for(i = left; i <= right; i++){
+            arr.set(i, tmp.get(i-left));
+        }
+    }
+}
