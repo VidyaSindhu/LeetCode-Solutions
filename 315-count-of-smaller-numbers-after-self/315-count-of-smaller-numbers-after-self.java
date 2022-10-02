05440a3473dbafe6ae59327c4298be0948f70a2d
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-02 19:04:04 +0530

    Time: 316 ms (33.96%), Space: 61.4 MB (91.30%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -1,59 +1,62 @@
 class Solution {
-    public void conquer(ArrayList<Pair<Integer, Integer>> numPairs, int left, int mid, int right, int[] result){
-        int count;
-        boolean found = false;
-        int size = right - left + 1;
-        ArrayList<Pair<Integer, Integer>> newTempNumPairs = new ArrayList<>();
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        List<Pair<Integer, Integer>> arr = new ArrayList();
+        for(int i = 0; i < n; i++){
+            Pair<Integer, Integer> pair = new Pair(nums[i], i);
+            arr.add(pair);
+        }
+        int[] count = new int[n];
+        mergeSort(arr, 0, n-1, count);
+        List<Integer> res = new ArrayList<Integer>();
+        
+        for(int num: count){
+            res.add(num);
+        }
+        
+        return res;
+    }
+    
+    void mergeSort(List<Pair<Integer, Integer>> arr, int left, int right, int[] res){
+        if(left < right){
+            int mid = left + (right-left)/2;
+            mergeSort(arr, left, mid, res);
+            mergeSort(arr, mid + 1, right, res);
+            merge(arr, left, right, res);
+        }
+    }
+    
+    void merge(List<Pair<Integer, Integer>> arr, int left, int right, int[] res){
+        int mid = left + (right-left)/2;
         int i = left, j = mid + 1;
-        int ptr = 0;
+        List<Pair<Integer, Integer>> tmp = new ArrayList();
+        int rightCount = 0;
         while(i <= mid && j <= right){
-            if(numPairs.get(i).getKey() > numPairs.get(j).getKey()){
-                result[numPairs.get(i).getValue()] += right - j + 1;
-                newTempNumPairs.add(numPairs.get(i));
+            if(arr.get(i).getKey() <= arr.get(j).getKey()){
+                res[arr.get(i).getValue()] += rightCount;
+                tmp.add(arr.get(i));
                 i++;
             }
             else{
-                newTempNumPairs.add(numPairs.get(j));
+                rightCount++;
+                tmp.add(arr.get(j));
                 j++;
             }
         }
+        
         while(i <= mid){
-            newTempNumPairs.add(numPairs.get(i));
+            res[arr.get(i).getValue()] += rightCount;
+            tmp.add(arr.get(i));
             i++;
         }
-        while(j <= right ){
-            newTempNumPairs.add(numPairs.get(j));
+        
+        while(j <= right){
+            tmp.add(arr.get(j));
             j++;
         }
-        for(i = 0; i < size; i++){
-            numPairs.set(i+left, newTempNumPairs.get(i));
-        }
         
-    }
-    
-    public void divide(ArrayList<Pair<Integer, Integer>> numPairs, int left, int right, int[] result){
-        int mid = (left + right)/2;
-        if(right > left){
-            divide(numPairs, left, mid, result);
-            divide(numPairs, mid+1, right, result);
-            conquer(numPairs, left, mid, right, result);
+        for(i = left; i <= right; i++){
+            arr.set(i, tmp.get(i-left));
         }
-    }
-    public List<Integer> countSmaller(int[] nums) {
-        int count = 0;
-        int n = nums.length;
-        int result[] = new int[nums.length];
-        ArrayList<Pair<Integer, Integer>> numPairs = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            numPairs.add(new Pair(nums[i], i));
-        }
-        divide(numPairs, 0, n-1, result);
-
-        List<Integer> list = new ArrayList<>();
-  
-        for(int i = 0; i < n; i++){
-            list.add(result[i]);
-        }
-        return list;
     }
 }
