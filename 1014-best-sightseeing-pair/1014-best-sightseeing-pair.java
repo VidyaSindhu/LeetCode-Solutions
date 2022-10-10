6c10d584a4259dc114f4ec7c20475393c09b78c9
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-10 16:32:31 +0530

    Time: 8 ms (32.68%), Space: 65.5 MB (10.65%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int n = values.length;
+        int[] valuesPlusI = new int[n];
+        valuesPlusI[0] = values[0];
+        
+        int[] maxValueMinusI = new int[n];
+        maxValueMinusI[n-1] = values[n-1] - (n-1);
+        for(int i = 1; i < n; i++){
+            valuesPlusI[i] = values[i] + i;
+            maxValueMinusI[n-i-1] = Integer.max(maxValueMinusI[n-i], values[n-i-1] - (n-i-1));
+            // System.out.print(maxValueMinusI[n-i-1] + " ");
+        }
+        
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n-1; i++){
+            ans = Integer.max(valuesPlusI[i] + maxValueMinusI[i+1], ans);
+        }
+        // System.out.print(maxValueMinusI[n-1] + " ");
+        return ans;
+    }
+}
