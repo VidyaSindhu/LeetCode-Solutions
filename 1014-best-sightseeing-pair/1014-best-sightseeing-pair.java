f02366ec46766b31816b6007169aa528b80855bd
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-10 16:45:54 +0530

    Time: 3 ms (91.95%), Space: 50.3 MB (97.89%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -7,13 +7,12 @@ class Solution {
         int[] maxValueMinusI = new int[n];
         maxValueMinusI[n-1] = values[n-1] - (n-1);
         for(int i = 1; i < n; i++){
-            valuesPlusI[i] = values[i] + i;
             maxValueMinusI[n-i-1] = Integer.max(maxValueMinusI[n-i], values[n-i-1] - (n-i-1));
         }
         
         int ans = Integer.MIN_VALUE;
         for(int i = 0; i < n-1; i++){
-            ans = Integer.max(valuesPlusI[i] + maxValueMinusI[i+1], ans);
+            ans = Integer.max(values[i] + i + maxValueMinusI[i+1], ans);
         }
         return ans;
     }
