4a558ec8e6b34e97fc5932fc05b6dbcde4582edf
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-23 11:31:14 +0530

    Time: 13 ms (33.57%), Space: 55 MB (25.59%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -6,9 +6,7 @@ class Solution {
             if(nums[idx-1] < 0) res[0] = idx;
             else nums[idx-1] *= -1;
         }
-        
-        // for(int num: nums) System.out.println(num + " ");
-        
+                
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > 0) res[1] = i+1;
         }
