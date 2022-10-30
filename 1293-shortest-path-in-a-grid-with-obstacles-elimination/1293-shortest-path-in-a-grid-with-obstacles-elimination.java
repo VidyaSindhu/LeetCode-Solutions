81176304772c19650af05cf9b6c96c4ee3913431
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-10-30 12:59:21 +0530

    Time: 72 ms (40.76%), Space: 53.5 MB (42.96%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -4,38 +4,37 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        Deque<int[]> queue = new ArrayDeque();
         boolean[][][] visited = new boolean[m][n][k+1];
+        Deque<int[]> queue = new ArrayDeque();
         
-        // x, y, steps, changes
         queue.offer(new int[]{0, 0, 0, k});
         
         visited[0][0][k] = true;
+        
         while(!queue.isEmpty()){
-            int x = queue.peek()[0];
-            int y = queue.peek()[1];
-            int steps = queue.peek()[2];
-            int changes = queue.poll()[3];
-            if(x == m-1 && y == n-1) return steps;
+            int[] top = queue.poll();
+            int steps = top[2];
+            if(top[0] == m-1 && top[1] == n-1) return steps;
             
-            steps++;                       
+            steps++;
+            int changes = top[3];
             
             for(int[] dir: dirs){
-                int newX = x + dir[0];
-                int newY = y + dir[1];
+                int newX = top[0] + dir[0];
+                int newY = top[1] + dir[1];
                 
                 if(newX < 0 || newY < 0 || newX >= m || newY >= n) continue;
                 
                 if(grid[newX][newY] == 1){
-                    if(changes > 0 && !visited[newX][newY][changes-1]) {
+                    if(changes > 0 && !visited[newX][newY][changes-1]){
                         queue.offer(new int[]{newX, newY, steps, changes-1});
                         visited[newX][newY][changes-1] = true;
                     }
                 }
-                else if(grid[newX][newY] == 0){
-                    if(visited[newX][newY][changes]) continue;
+                else if(!visited[newX][newY][changes]){
                     queue.offer(new int[]{newX, newY, steps, changes});
                     visited[newX][newY][changes] = true;
+                    
                 }
             }
         }
