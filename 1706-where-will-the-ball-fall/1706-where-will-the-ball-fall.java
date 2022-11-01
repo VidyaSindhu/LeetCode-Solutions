913ed0f13fe64cc6c6db0789a490608feadd6d41
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-01 14:07:21 +0530

    Time: 1 ms (96.80%), Space: 54 MB (72.75%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int n = grid[0].length;
+        int[] res = new int[n];
+        
+        for(int j = 0; j < n; j++){
+            res[j] = dfs(grid, 0, j);
+        }
+        
+        return res;
+    }
+    
+    int dfs(int[][] grid, int x, int y){
+        if(x == grid.length) return y;
+        
+        int ans = -1;
+        if(grid[x][y] == 1){
+            if(y < grid[0].length-1 && grid[x][y+1] == 1) ans = dfs(grid, x+1, y+1);
+        }
+        else if(grid[x][y] == -1){
+            if(y > 0 && grid[x][y-1] == -1) ans = dfs(grid, x+1, y-1);
+        }
+        
+        
+        return ans;
+    }
+}
