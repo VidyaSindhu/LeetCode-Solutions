70094d6eca3eb47735d2aa1512a66a4b737193dc
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-08 11:40:06 +0530

    Time: 98 ms (58.33%), Space: 63 MB (88.10%) - LeetHub

--- 1889-minimum-space-wasted-from-packaging/1889-minimum-space-wasted-from-packaging.java
+++ 1889-minimum-space-wasted-from-packaging/1889-minimum-space-wasted-from-packaging.java
@@ -3,29 +3,24 @@ class Solution {
     public int minWastedSpace(int[] packages, int[][] boxes) {
         Arrays.sort(packages);
         int n = packages.length;
-        long[] prefix = new long[n+1];
+        long totalSum = 0;
         
-        for(int i = 1; i < n+1; i++){
-            prefix[i] = prefix[i-1] + packages[i-1];
-        }
-        
-        for(int[] box: boxes){
-            Arrays.sort(box);
+        for(int i = 0; i < n; i++){
+            totalSum += packages[i];
         }
         
         long ans = Long.MAX_VALUE;
         for(int[] box: boxes){
-            
+            Arrays.sort(box);
             if(box[box.length-1] < packages[n-1]) continue;
             int last = 0;
             
             long waste = 0;
+            long len = 0;
             for(int i = 0; i < box.length; i++){
                 int ceil = ceil(packages, box[i]);
                 if(ceil != -1){
-                    // long segmentSum = prefix[ceil+1] - prefix[last];
-                    long len = ceil - last + 1;
-                    // waste += (len * (long)box[i] - segmentSum) % MOD;
+                    len = ceil - last + 1;
                     waste += len * box[i];
                     last = ceil+1;
                     if(ceil >= packages.length) break;
@@ -35,7 +30,7 @@ class Solution {
             ans = Long.min(ans, waste);
         }
         
-        return (int)(ans >= Long.MAX_VALUE ? -1 : (ans - prefix[prefix.length-1]) % MOD);
+        return (int)(ans >= Long.MAX_VALUE ? -1 : (ans - totalSum) % MOD);
     }
     
     int ceil(int[] nums, int target){
