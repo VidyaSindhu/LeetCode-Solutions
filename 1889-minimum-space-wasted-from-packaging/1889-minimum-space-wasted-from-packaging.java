109b14c5b2f97c15d8650422ad652e7d9c14f5d6
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-08 11:37:58 +0530

    Time: 97 ms (60.71%), Space: 63 MB (88.10%) - LeetHub

---
+++ 1889-minimum-space-wasted-from-packaging/1889-minimum-space-wasted-from-packaging.java
@@ -0,0 +1,53 @@
+class Solution {
+    int MOD = 1_000_000_007;
+    public int minWastedSpace(int[] packages, int[][] boxes) {
+        Arrays.sort(packages);
+        int n = packages.length;
+        long[] prefix = new long[n+1];
+        
+        for(int i = 1; i < n+1; i++){
+            prefix[i] = prefix[i-1] + packages[i-1];
+        }
+        
+        for(int[] box: boxes){
+            Arrays.sort(box);
+        }
+        
+        long ans = Long.MAX_VALUE;
+        for(int[] box: boxes){
+            
+            if(box[box.length-1] < packages[n-1]) continue;
+            int last = 0;
+            
+            long waste = 0;
+            for(int i = 0; i < box.length; i++){
+                int ceil = ceil(packages, box[i]);
+                if(ceil != -1){
+                    // long segmentSum = prefix[ceil+1] - prefix[last];
+                    long len = ceil - last + 1;
+                    // waste += (len * (long)box[i] - segmentSum) % MOD;
+                    waste += len * box[i];
+                    last = ceil+1;
+                    if(ceil >= packages.length) break;
+                }
+            }
+            
+            ans = Long.min(ans, waste);
+        }
+        
+        return (int)(ans >= Long.MAX_VALUE ? -1 : (ans - prefix[prefix.length-1]) % MOD);
+    }
+    
+    int ceil(int[] nums, int target){
+        int low = 0, high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] <= target){
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        
+        return high;
+    }
+}
