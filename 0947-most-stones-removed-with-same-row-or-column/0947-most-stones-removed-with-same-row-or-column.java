72ad3ca5c8437da0c1adcb2b01f7c3c2304b4387
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-15 10:22:27 +0530

    Time: 39 ms (70.75%), Space: 48.9 MB (70.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,27 +1,41 @@
 class Solution {
-    Map<Integer, Integer> f = new HashMap<>();
-    int islands = 0;
-
     public int removeStones(int[][] stones) {
-        for (int i = 0; i < stones.length; ++i)
-            union(stones[i][0], ~stones[i][1]);
-        return stones.length - islands;
+        int n = stones.length;
+        int[] parents = new int[n];
+        Arrays.fill(parents, -1);
+        
+        int[] setSize = new int[n];
+        Arrays.fill(setSize, 1);
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    union(parents, i, j, setSize);
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(parents[i] == -1){
+                ans += setSize[i] - 1;
+            }
+        }
+        
+        return ans;
     }
-
-    public int find(int x) {
-        if (f.putIfAbsent(x, x) == null)
-            islands++;
-        if (x != f.get(x))
-            f.put(x, find(f.get(x)));
-        return f.get(x);
+    
+    int find(int[] parents, int i){
+        if(parents[i] == -1) return i;
+        return parents[i] = find(parents, parents[i]);
     }
-
-    public void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) {
-            f.put(x, y);
-            islands--;
+    
+    void union(int[] parents, int x, int y, int[] setSize){
+        int p1 = find(parents, x);
+        int p2 = find(parents, y);
+        
+        if(p1 != p2){
+            parents[p1] = p2;
+            setSize[p2] += setSize[p1];
         }
     }
 }
