02aa3756268129c699bdbfc38a761d83e7df9e08
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-21 11:07:37 +0530

    Time: 15 ms (60.92%), Space: 72.4 MB (33.43%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,32 +1,40 @@
 class Solution {
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int nearestExit(char[][] maze, int[] entrance) {
         int m = maze.length;
         int n = maze[0].length;
-        Deque<int[]> queue = new ArrayDeque();
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         
-        int idx = entrance[0] * n + entrance[1];
-        queue.offer(new int[]{idx, 0});
+        
+        Deque<int[]> queue = new ArrayDeque();
+        queue.offer(new int[]{entrance[0], entrance[1], 0});
         maze[entrance[0]][entrance[1]] = '+';
+        
         while(!queue.isEmpty()){
-            idx = queue.peek()[0];
-            int x = idx/n;
-            int y = idx - x * n;
-            int steps = queue.peek()[1];
+            int[] top = queue.poll();
+            // for(int num: top){
+            //     System.out.print(num + "  ");
+            // }
+            // System.out.println("");
+            int x = top[0];
+            int y = top[1];
+            int steps = top[2];            
             
-            queue.poll();
-            // System.out.println(x + " " + y);
             for(int[] dir: dirs){
                 int newX = x + dir[0];
                 int newY = y + dir[1];
-                int newIdx = newX * n + newY;
-                if(newX < 0 || newY < 0 || newX >= m || newY >= n || maze[newX][newY] != '.') continue;
-                if(newX == 0 || newX == m-1 || newY == 0 || newY == n-1) return steps+1;
+                
+                if(newX < 0 || newY < 0 || newX >= m || newY >= n || maze[newX][newY] == '+') continue;
+                
+                if(newX == 0 || newY == 0 || newX == m-1 || newY == n-1) return steps+1;
+                
+                queue.offer(new int[]{newX, newY, steps+1});
                 maze[newX][newY] = '+';
-                queue.offer(new int[]{newIdx, steps+1});
             }
         }
         
-        return -1;        
+        return -1;
+        
+        
+        
     }
 }
