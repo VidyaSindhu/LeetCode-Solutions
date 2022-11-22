304ead866d46a10b800a173c7a1664b36106244f
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-22 11:29:16 +0530

    Time: 101 ms (39.70%), Space: 44.2 MB (28.58%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,32 +1,21 @@
 class Solution {
     public int numSquares(int n) {
-        int[] squares = new int[100];
+        int[] squares = new int[101];
         for(int i = 1; i < 101; i++){
-            squares[i-1] = i*i;
+            squares[i] = i*i;
         }
         
-        int memo[][] = new int[100+1][n+1];
-        
-        for(int i = 0; i < n+1; i++){
-            memo[0][i] = Integer.MAX_VALUE;
-            memo[1][i] = i;
-        }
-        
-        for(int i = 0; i < 101; i++){
-            memo[i][0] = 0;
-        }
-        
-        for(int i = 1; i < 101; i++){
-            for(int j = 1; j < n+1; j++){
-                if(squares[i-1] <= j){
-                    memo[i][j] = Math.min(memo[i-1][j], memo[i][j-squares[i-1]] + 1);
-                }
-                else{
-                    memo[i][j] = memo[i-1][j];
+        int[] memo = new int[n+1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < 101; j++){
+                if(i >= squares[j]){
+                    memo[i] = Integer.min(memo[i], memo[i-squares[j]] + 1);
                 }
             }
         }
         
-        return memo[100][n];
+        return memo[n];
     }
 }
