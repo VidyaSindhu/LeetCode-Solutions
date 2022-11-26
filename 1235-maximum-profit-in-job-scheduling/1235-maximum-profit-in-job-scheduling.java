f3806f8c12cfdec305c40ce78e847c383943fc13
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-11-27 02:47:04 +0530

    Time: 70 ms (53.77%), Space: 50 MB (97.41%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,47 +1,26 @@
 class Solution {
-    int[] tree;
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> endTime[a] - endTime[b]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            // if(endTime[a] == endTime[b]) return startTime[];
+            return endTime[a] - endTime[b];
+        });
         
-        for(int i = 0; i < n; i++){
-            heap.offer(i);
+        for(int i = 0; i < endTime.length; i++){
+            pq.offer(i);
         }
-        tree = new int[n+1];
-        HashMap<Integer, Integer> map = new HashMap();
-        TreeMap<Integer, Integer> treeMap = new TreeMap();
-        for(int i = 0; i < n; i++){
-            int max = 0;
-            int idx = heap.poll();
-            map.put(i, endTime[idx]);
-            Integer floor = treeMap.floorKey(startTime[idx]);
-            if(floor != null){
-                max = getMax(treeMap.get(floor));
-            }
-            treeMap.put(endTime[idx], Integer.max(treeMap.getOrDefault(endTime[idx], 0), i));
-            build(max + profit[idx], i);
-        }
+        TreeMap<Integer, Integer> dp = new TreeMap();
         
-        
-        return getMax(n-1);        
-    }
-    
-    void build(int val, int x){
-        x++;
-        while(x < tree.length){
-            tree[x] = Integer.max(tree[x], val);
-            x += (-x & x);
-        }
-    }
-    
-    int getMax(int x){
+        dp.put(-1, 0);
         int ans = 0;
-        x++;
-        while(x > 0){
-            ans = Integer.max(ans, tree[x]);
-            x -= (-x & x);
+        while(!pq.isEmpty()){
+            int idx = pq.poll();
+            int max = dp.floorEntry(startTime[idx]).getValue() + profit[idx];
+            if(max > dp.lastEntry().getValue()){
+                dp.put(endTime[idx], max);
+            }
+            ans = Integer.max(ans, max);
         }
+        
         return ans;
     }
-    
 }
