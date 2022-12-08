b1a3e9c0ee51ed39e0fbabea17638fae09df075d
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-08 23:54:07 +0530

    Time: 19 ms (36.67%), Space: 82.7 MB (25.45%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.java
+++ 0565-array-nesting/0565-array-nesting.java
@@ -1,24 +1,22 @@
 class Solution {
     public int arrayNesting(int[] nums) {
         int n = nums.length;
-        // boolean[] visited = new boolean[n];
         
         int ans = 0;
         for(int i = 0; i < n; i++){
             if(nums[i] == -1) continue;
             int tmp = nums[i];
             nums[i] = -1;
-            ans = Integer.max(ans, dfs(nums, tmp, nums[i], 0));
+            ans = Integer.max(ans, dfs(nums, tmp, 0));
         }
         
         return ans;
     }
     
-    int dfs(int[] nums, int i, int p, int depth){
+    int dfs(int[] nums, int i, int depth){
         if(i == -1) return depth;
-        if(nums[i] == -1) return depth+1;
         int tmp = nums[i];
         nums[i] = -1;
-        return dfs(nums, tmp, p, depth+1);
+        return dfs(nums, tmp, depth+1);
     }
 }
