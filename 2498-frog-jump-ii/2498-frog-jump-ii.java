e3f592ab6e5476b05e11813d6eae37c3576033b2
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-11 01:09:09 +0530

    Time: 2 ms (100.00%), Space: 55.9 MB (100.00%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.java
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -1,55 +1,10 @@
 class Solution {
-    boolean[] visited;
     public int maxJump(int[] stones) {
-        int n = stones.length;
-        int low = 0, high = stones[n-1] - stones[0];
-        visited = new boolean[n];
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(isPossible(stones, mid)){
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+        int ans = stones[1] - stones[0];
+        for(int i = 1; i < stones.length-1; i++){
+            ans = Integer.max(ans, stones[i+1] - stones[i-1]);
         }
         
-        return low;
-    }
-    
-    boolean isPossible(int[] stones, int jump){
-        int n = stones.length;
-        Arrays.fill(visited, false);
-        
-        int curr = 0;
-
-        for(int i = 0; i < stones.length-1; i++){
-            curr = i + 1;
-            while(curr < stones.length && stones[curr] - stones[i] <= jump){
-                curr++;
-            }
-            if(i == curr-1) return false;
-            if(curr == stones.length) break;
-            visited[curr-1] = true;
-            i = curr-2;
-        }
-        
-        int start = stones.length - 1;
-        int prev = start;
-        for(int i = stones.length-2; i >= 0;){
-            if(visited[i]){
-                i--;
-                continue;
-            }
-            if(stones[start] - stones[i] > jump) {
-                if(start == prev) return false;
-                start = prev;
-            }
-            else{
-                prev = i;
-                i--;
-            }
-        }
-        return true;
+        return ans;
     }
 }
