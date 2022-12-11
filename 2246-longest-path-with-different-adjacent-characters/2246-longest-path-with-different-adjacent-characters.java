59a7bfdd169cc75482cc0e120661f8a24f77fc4b
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-11 11:36:19 +0530

    Time: 506 ms (7.73%), Space: 255.7 MB (14.83%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -7,8 +7,7 @@ class Solution {
             adj.putIfAbsent(parent[i], new ArrayList());
             adj.get(parent[i]).add(i);
         }
-        // System.out.println(adj);
-        // ans = Integer.max(ans, );
+        
         dfs(adj, -1, s);
         return ans;        
     }
@@ -16,7 +15,6 @@ class Solution {
     int dfs(Map<Integer, List<Integer>> adj, int node, String s){
         if(adj.get(node) == null || adj.get(node).isEmpty()) return 0;
         
-        int lenLeft = 0, lenRight = 0;
         List<Integer> list = new ArrayList();
         for(int child: adj.get(node)){
             if(node != -1 && s.charAt(child) != s.charAt(node)){
@@ -26,23 +24,8 @@ class Solution {
                 dfs(adj, child, s);
             }
         }
-//         if(adj.get(node).size() > 0){
-//             int child = adj.get(node).get(0);
-            
-//         }
-        
-//         if(adj.get(node).size() > 1){
-//             int child = adj.get(node).get(1);
-//             if(node != -1 && s.charAt(child) != s.charAt(node)){
-//                 lenRight += 1 + dfs(adj, child, s);
-//             }
-//             else{
-//                 dfs(adj, child, s);
-//             }
-//         }
-//         System.out.println(node + " " + lenLeft + " " + lenRight);
+
         Collections.sort(list, (a, b) -> b-a);
-        // ans = Integer.max(ans, lenLeft + lenRight + 1);
         int size = Integer.min(list.size(), 2);
         int len = 0;
         int i = 0;
