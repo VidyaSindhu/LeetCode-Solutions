5612c1065e758e3493167dc32a3723d14d4ba073
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-13 08:25:57 +0530

    Time: 21 ms (51.34%), Space: 41.7 MB (50.80%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -18,20 +18,4 @@ class Solution {
         long ans = prevHousePlaced + prevHouseNotPlaced;
         return (int)(((ans % MOD) * (ans % MOD)) % MOD);
     }
-    
-//     long helper(int n, int prevHouse, int i){
-//         if(i >= n) return 1;
-        
-//         long ans = 0;
-//         if(dp[i][prevHouse] != null) return dp[i][prevHouse];
-        
-//         if(prevHouse == 1){
-//             ans = helper(n, prevHouse ^ 1, i+1) % MOD;;
-//         }
-//         else{
-//             ans = (helper(n, prevHouse, i+1) % MOD + helper(n, prevHouse ^ 1, i+1) % MOD) % MOD;
-//         }
-        
-//         return dp[i][prevHouse] = (int)ans;
-//     }
 }
