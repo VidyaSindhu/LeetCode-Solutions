8a3947be8bff8ab3bf188794a3792bc27f2fa90e
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-18 14:38:33 +0530

    Time: 499 ms (33.33%), Space: 158.9 MB (100.00%) - LeetHub

---
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] cycleLengthQueries(int n, int[][] queries) {
+        int[] res = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            Stack<Integer> p1 = getPath(queries[i][0]);
+            Stack<Integer> p2 = getPath(queries[i][1]);
+            
+            while(!p1.isEmpty() && !p2.isEmpty() && p1.peek().equals(p2.peek())){
+                p1.pop();
+                p2.pop();
+            }
+            
+            res[i] = p1.size() + p2.size() + 1;
+            if(res[i] == 15) System.out.println(queries[i][0] + " " + queries[i][1]);
+        }
+        return res;
+    }
+    
+    Stack<Integer> getPath(int n){
+        Stack<Integer> stack = new Stack();
+        for(; n > 0; ){
+            stack.push(n);
+            if(n % 2 == 0) n /= 2;
+            else n = (n-1)/2;
+        }
+        
+        return stack;
+    }
+}
