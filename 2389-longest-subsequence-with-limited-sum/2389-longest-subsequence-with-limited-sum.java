0c40246811b58f025b5c6a9d7ce5aaae41e947eb
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-25 13:28:03 +0530

    Time: 5 ms (97.08%), Space: 42.8 MB (92.57%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
-        int n = queries.length;
-        int[] res = new int[n];
-        
-        for(int i = 0; i < n; i++){
-            int sum = 0;
-            for(int num: nums){
-                sum += num;
-                pq.offer(num);
-                while(!pq.isEmpty() && sum > queries[i]){
-                    sum -= pq.poll();
-                }
-            }
-            res[i] = pq.size();
-            pq.clear();
+        int n = nums.length;
+        int[] prefix = new int[n];
+        Arrays.sort(nums);
+        prefix[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + nums[i];
         }
+        
+        int[] res = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            int insertionIndex = binarySearch(prefix, queries[i]);
+            res[i] = insertionIndex;
+        }
+        
         return res;
+    }
+    
+    int binarySearch(int[] nums, int target){
+        int low = 0, high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] <= target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        
+        return low;
     }
 }
