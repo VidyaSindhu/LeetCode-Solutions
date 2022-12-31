621504ab8f0776b4f134c157a42276a6652b31c4
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2022-12-31 11:14:58 +0530

    Time: 1 ms (85.48%), Space: 39.9 MB (78.34%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -0,0 +1,49 @@
+class Solution {
+    int ans = 0;
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public int uniquePathsIII(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int zeroSquares = 0;
+        int start[] = new int[2], end[] = new int[2];
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    start[0] = i;
+                    start[1] = j;
+                }
+                else if(grid[i][j] == 2){
+                    end[0] = i;
+                    end[1] = j;
+                }
+                else if(grid[i][j] == 0) {
+                    zeroSquares++;
+                }
+            }
+        }
+        // System.out.println(zeroSquares);
+        dfs(grid, zeroSquares+1, start[0], start[1], end);
+            
+        return ans;
+    }
+    
+    void dfs(int[][] grid, int zeroSquares, int x, int y, int[] end){
+        if(grid[x][y] == 2){
+            if(zeroSquares == 0) ans++;
+            return;
+        }
+        int tmp = grid[x][y];
+        grid[x][y] = -1;
+        for(int[] dir: dirs){
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            
+            if(newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length 
+              || grid[newX][newY] == -1) continue;
+            
+            dfs(grid, zeroSquares-1, newX, newY, end);
+        }
+        grid[x][y] = tmp;
+    }
+}
