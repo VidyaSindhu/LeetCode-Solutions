0be93403c3c911364bf863c97c8f7a784bf21512
Vidya Sindhu Dubey <74533961+VidyaSindhu@users.noreply.github.com>
2023-01-03 12:34:18 +0530

    Time: 70 ms (55.94%), Space: 55.6 MB (79.21%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -4,13 +4,6 @@ class Solution {
     public int countPaths(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> grid[b[0]][b[1]] - grid[a[0]][a[1]]);
-        
-//         for(int i = 0; i < m; i++){
-//             for(int j = 0; j < n; j++){
-//                 pq.offer(new int[]{i, j});
-//             }
-//         }
         
         long ans = 0l;
         Long[][] dp = new Long[m][n];
@@ -19,30 +12,6 @@ class Solution {
                 ans = (ans + helper(grid, i, j, dp)) % MOD;
             }
         }
-        // long[][] dp = new long[m][n];
-        // for(long[] row: dp){
-        //     Arrays.fill(row, 1l);
-        // }
-        
-//         while(!pq.isEmpty()){
-//             int[] top = pq.poll();
-//             int x = top[0];
-//             int y = top[1];
-//             for(int[] dir: dirs){
-//                 int newX = x + dir[0];
-//                 int newY = y + dir[1];
-//                 if(newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length) continue;
-                
-//                 if(grid[x][y] > grid[newX][newY]){
-//                     dp[newX][newY] = (dp[newX][newY] + dp[x][y]) % MOD;
-//                 }
-//             }
-//         }
-        // for(long row[]: dp){
-        //     for(long num: row){
-        //         ans = (ans + num) % MOD;
-        //     }
-        // }
         
         return (int)ans;
     }
